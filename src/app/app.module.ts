2e001db805b674339c3decba8afa420a68e0adfa
pawelkat <pawel.kreski@yahoo.com>
2019-03-17 19:57:30 +0100

    somewhere in the middle of the course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from 'src/shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/product.ts
@@ -0,0 +1,9 @@
+
+export interface IProduct {
+  id: number;
+  name: string;
+  code: string;
+  imageUrl?: string;
+  price?: number;
+  currency?: string;
+}
